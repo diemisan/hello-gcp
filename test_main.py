be033443965dcf379fde080d197d2c53ccb217a2
Diego Miñano <d.minano@ptytechnologies.com>
2024-12-24 18:58:04 +0100

    fix test

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import dummy
 class TestMain(unittest.TestCase):
     def test_dummy(self):
         result = dummy(1)
-        self.assertEqual(result, 2)
+        self.assertEqual(result, 1)
 
 if __name__ == '__main__':
     unittest.main()
